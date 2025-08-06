f9860f27f0101e2bc7ce8d49f4490ca5ad855c09
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-08-06 13:46:07 +0530

    react-router-dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Login from './component/Login';
+import Register from './component/Register';
 
 function App() {
   
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path : "/",
+      element : <Login />
+    },
+    {
+      path : "/register",
+      element : <Register />
+    },
+  ])
 
   return (
     <>
-        
+      <RouterProvider router={router} />
     </>
   )
 }
